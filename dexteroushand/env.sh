6e1a28689679388eb089be28ffc2ffe773921cbd
dachii-azm <alohadaichi@gmail.com>
2025-09-03 17:55:16 +0900

    feat: remove files

--- dexteroushand/env.sh
+++
@@ -1,2 +0,0 @@
-export X11_FORWARDING_ENABLED=1
-export TMP_XAUTH=/tmp/tmp.XyBIT2tLEt/tmp.zIePE67qIW.xauth
--- isaaclab/env.sh
+++
@@ -1,2 +0,0 @@
-export x11_forwarding_enabled = 1
-export __isaaclab_tmp_xauth = /tmp/tmp.XyBIT2tLEt/tmp.zIePE67qIW.xauth
